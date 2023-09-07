5536bea5cf80f53904c248ffdd0fba6209e5ba7b
adzibilal <adzibilal02@gmail.com>
2023-09-07 20:23:35 +0700

    font-chakra

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1 className='text-4xl'>Hai ini web porto baru adzi</h1>
+     <h1 className='text-4xl font-chakra font-extrabold'>Hai ini web porto baru adzi</h1>
     </main>
   )
 }
