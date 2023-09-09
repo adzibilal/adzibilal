1efd67336568cee1dee0b2da280bb97156514440
adzibilal <adzibilal02@gmail.com>
2023-09-09 15:31:55 +0700

    move exp to constants

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,3 +1,5 @@
+import { educationData, workExperience } from "@/constants/experienceData";
+
 export default function Experience() {
     return (
         <section className='max-container grid grid-cols-2 gap-10 max-lg:!mx-5 max-lg:grid-cols-1'>
@@ -5,91 +7,44 @@ export default function Experience() {
                 <div className='title-section mb-8'>
                     <span className='text-gradient'>Work Experience</span>
                 </div>
-                <div className='item-exp'>
-                    <div className='flex gap-5 items-center'>
-                        <img
-                            src='/images/jumpa.id.png'
-                            alt=''
-                            className='img-exp'
-                        />
-
-                        <div className=''>
-                            <div className='title'>Jumpa ID</div>
-                            <div className='prod'>Frontend Developer</div>
+                {workExperience.map((exp, index) => (
+                    <div key={index} className='item-exp'>
+                        <div className='flex gap-5 items-center'>
+                            <img
+                                src={exp.imageSrc}
+                                alt={exp.company}
+                                className='img-exp'
+                            />
+                            <div className=''>
+                                <div className='title'>{exp.company}</div>
+                                <div className='prod'>{exp.position}</div>
+                            </div>
                         </div>
+                        <p>{exp.period}</p>
                     </div>
-
-                    <p>2022 - Now</p>
-                </div>
-                <div className='item-exp'>
-                    <div className='flex gap-5 items-center'>
-                        <img
-                            src='/images/bara.png'
-                            alt=''
-                            className='img-exp'
-                        />
-
-                        <div className=''>
-                            <div className='title'>Bara Enterprise</div>
-                            <div className='prod'>Frontend Developer</div>
-                        </div>
-                    </div>
-
-                    <p>2020 - 2022</p>
-                </div>
-                <div className='item-exp'>
-                    <div className='flex gap-5 items-center'>
-                        <img
-                            src='/images/bara.png'
-                            alt=''
-                            className='img-exp'
-                        />
-
-                        <div className=''>
-                            <div className='title'>Bara Enterprise</div>
-                            <div className='prod'>Frontend Developer (Intern) </div>
-                        </div>
-                    </div>
-
-                    <p>2019 - 2020</p>
-                </div>
+                ))}
             </div>
+
             <div className='experience-box'>
                 <div className='title-section mb-8'>
                     <span className='text-gradient'>Education</span>
                 </div>
-                <div className='item-exp'>
-                    <div className='flex gap-5 items-center'>
-                        <img
-                            src='/images/sttb.png'
-                            alt=''
-                            className='img-exp'
-                        />
-
-                        <div className=''>
-                            <div className='title'>Sekolah Tinggi Teknologi Bandung</div>
-                            <div className='prod'>Tenkik Informatikak</div>
+                {educationData.map((edu, index) => (
+                    <div key={index} className='item-exp'>
+                        <div className='flex gap-5 items-center'>
+                            <img
+                                src={edu.imageSrc}
+                                alt={edu.school}
+                                className='img-exp'
+                            />
+                            <div className=''>
+                                <div className='title'>{edu.school}</div>
+                                <div className='prod'>{edu.major}</div>
+                            </div>
                         </div>
+                        <p>{edu.period}</p>
                     </div>
-
-                    <p>2017-2020</p>
-                </div>
-                <div className='item-exp'>
-                    <div className='flex gap-5 items-center'>
-                        <img
-                            src='/images/smk2cimahi.jpg'
-                            alt=''
-                            className='img-exp'
-                        />
-
-                        <div className=''>
-                            <div className='title'>SMK Negri 2 Cimahi</div>
-                            <div className='prod'>Rekayasa Perangkat Lunak</div>
-                        </div>
-                    </div>
-
-                    <p>2017-2020</p>
-                </div>
+                ))}
             </div>
         </section>
     )
---
+++ constants/experienceData.ts
@@ -0,0 +1,38 @@
+// constants/experienceData.ts
+
+export const workExperience = [
+    {
+      company: 'Jumpa ID',
+      position: 'Frontend Developer',
+      imageSrc: '/images/jumpa.id.png',
+      period: '2022 - Now',
+    },
+    {
+      company: 'Bara Enterprise',
+      position: 'Frontend Developer',
+      imageSrc: '/images/bara.png',
+      period: '2020 - 2022',
+    },
+    {
+      company: 'Bara Enterprise',
+      position: 'Frontend Developer (Intern)',
+      imageSrc: '/images/bara.png',
+      period: '2019 - 2020',
+    },
+  ];
+  
+  export const educationData = [
+    {
+      school: 'Sekolah Tinggi Teknologi Bandung',
+      major: 'Teknik Informatika',
+      imageSrc: '/images/sttb.png',
+      period: '2017 - 2020',
+    },
+    {
+      school: 'SMK Negri 2 Cimahi',
+      major: 'Rekayasa Perangkat Lunak',
+      imageSrc: '/images/smk2cimahi.jpg',
+      period: '2017 - 2020',
+    },
+  ];
+  
