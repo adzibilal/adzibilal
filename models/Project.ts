672e22d92016774ad590d9129ffa4a66acd8ef12
adzibilal <adzibilal02@gmail.com>
2023-09-10 18:40:29 +0700

    update model

--- models/Project.ts
+++ models/Project.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Schema } from 'mongoose'
+import mongoose, { Document, Schema, Model } from 'mongoose'
 
 export interface IProject extends Document {
     judul: string
@@ -9,13 +9,23 @@ export interface IProject extends Document {
     link: string
 }
 
-const projectSchema = new Schema<IProject>({
-    judul: { type: String, required: true },
-    deskripsi: { type: String, required: true },
-    image: { type: String, required: true },
-    content: { type: String, required: true },
-    teknologi: [{ type: String, required: true }],
-    link: { type: String, required: true }
-})
+// Periksa apakah model Project sudah ada sebelumnya
+let Project: Model<IProject>
 
-export default mongoose.model<IProject>('Project', projectSchema)
+try {
+    Project = mongoose.model<IProject>('Project')
+} catch (error) {
+    // Jika model belum ada, maka kompilasi model baru
+    const projectSchema = new Schema<IProject>({
+        judul: { type: String, required: true },
+        deskripsi: { type: String, required: true },
+        image: { type: String, required: true },
+        content: { type: String, required: true },
+        teknologi: [{ type: String, required: true }],
+        link: { type: String, required: true }
+    })
+
+    Project = mongoose.model<IProject>('Project', projectSchema)
+}
+
+export default Project
