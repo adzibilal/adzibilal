efb1e29eb6e2efb71a7008dc5ec73862dff63948
adzibilal <adzibilal02@gmail.com>
2023-09-15 14:31:21 +0700

    login admin

--- pages/admin/layout.tsx
+++ pages/admin/layout.tsx
@@ -2,11 +2,53 @@ import '@/app/globals.css'
 import './admin.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import jwt from 'jsonwebtoken'
+
 export default function AdminLayout({
     children
 }: {
     children: React.ReactNode
 }) {
+    const router = useRouter()
+
+    useEffect(() => {
+        // Pengecekan token di sini
+        const token = localStorage.getItem('token') // Gantilah dengan kunci yang sesuai
+
+        if (!token) {
+            // Jika tidak ada token, arahkan ke halaman /login
+            router.push('/login')
+        }
+
+        // Memeriksa waktu kadaluarsa token (expire time)
+        try {
+            //@ts-ignore
+            const decodedToken = jwt.decode(token)
+
+            if (decodedToken) {
+                const currentTimestamp = Math.floor(Date.now() / 1000)
+
+                if (decodedToken.exp && decodedToken.exp < currentTimestamp) {
+                    // Token telah kadaluwarsa, arahkan ke /login
+                    router.push('/login')
+                }
+            }
+        } catch (error) {
+            console.error('Error decoding token:', error)
+            // Handle error decoding token if needed
+        }
+    }, [])
+
+    // Handler untuk logout
+    const handleLogout = () => {
+        // Hapus token dari localStorage
+        localStorage.removeItem('token')
+
+        // Redirect ke halaman /login
+        router.push('/login')
+    }
     return (
         <section>
             <div className='navbar bg-base-100'>
@@ -26,17 +68,7 @@ export default function AdminLayout({
                             <Link href='/admin/projects'>Projects</Link>
                         </li>
                         <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className='p-2 bg-base-100'>
-                                    <li>
-                                        <a>Link 1</a>
-                                    </li>
-                                    <li>
-                                        <a>Link 2</a>
-                                    </li>
-                                </ul>
-                            </details>
+                            <div className='text-error hover:!text-error' onClick={handleLogout}>Logout</div>
                         </li>
                     </ul>
                 </div>
---
+++ pages/login/index.tsx
@@ -0,0 +1,99 @@
+import { NextPage } from 'next'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import '@/app/globals.css'
+import LoadingOverlay from '@/components/LoadingOverlay'
+
+interface Props {}
+
+const Index: NextPage<Props> = ({}) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState('')
+    const router = useRouter()
+
+    useEffect(() => {
+        // Cek apakah data sesi sudah ada di localStorage
+        const token = localStorage.getItem('token')
+        if (token) {
+            router.push('/admin') // Redirect ke halaman '/admin' jika token sudah ada
+        }
+    }, [])
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault() // Mencegah pengiriman formulir standar
+        setIsLoading(true)
+
+        // Simpan kode autentikasi di sini, misalnya menggunakan fetch atau axios
+
+        // Misalnya, Anda dapat menggunakan fetch
+        try {
+            const response = await fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            })
+
+            if (response.ok) {
+                // Autentikasi berhasil, arahkan pengguna ke halaman selanjutnya
+                const data = await response.json()
+                localStorage.setItem('token', data.token)
+                router.push('/admin')
+            } else {
+                const data = await response.json()
+                setError(data.error || 'Terjadi kesalahan saat login')
+            }
+        } catch (error) {
+            setError('Terjadi kesalahan saat login')
+        }
+
+        setIsLoading(false)
+    }
+
+    return (
+        <div className='flex items-center justify-center w-full h-screen'>
+            <div className='w-full max-w-[500px]'>
+                <form onSubmit={handleLogin}>
+                    <div className='mb-4'>
+                        <div className='form-control w-full'>
+                            <label className='label'>
+                                <span className='label-text'>Username</span>
+                            </label>
+                            <input
+                                type='text'
+                                value={username}
+                                onChange={e => setUsername(e.target.value)}
+                                placeholder='Username'
+                                className='input input-bordered w-full'
+                            />
+                        </div>
+                    </div>
+                    <div className='mb-4'>
+                        <div className='form-control w-full'>
+                            <label className='label'>
+                                <span className='label-text'>Username</span>
+                            </label>
+                            <input
+                                type='password'
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                placeholder='Password'
+                                className='input input-bordered w-full'
+                            />
+                        </div>
+                    </div>
+                   
+                    <button className='btn w-full' type='submit' disabled={isLoading}>
+                        {isLoading ? 'Logging in...' : 'Login'}
+                    </button>
+                    {error && <p className='text-error mt-3 text-center'>{error}</p>}
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Index
