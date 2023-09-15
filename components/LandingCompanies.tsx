74da4e15c77f652f43aada2fe9cf287ba05b21c6
adzibilal <adzibilal02@gmail.com>
2023-09-15 14:50:36 +0700

    change brands

--- components/LandingCompanies.tsx
+++ components/LandingCompanies.tsx
@@ -1,16 +1,17 @@
+import { companies } from '@/constants/brands' // Sesuaikan dengan path menuju file brands.ts
+
 export default function LandingCompanies() {
     return (
-        <section className="max-container max-lg:!mx-5">
-            <div className="font-chakra text-6xl font-extrabold text-center mb-20 max-sm:text-5xl"><span className="text-gradient">Companies</span> I've Worked with</div>
+        <section className='max-container max-lg:!mx-5'>
+            <div className='font-chakra text-6xl font-extrabold text-center mb-20 max-sm:text-5xl'>
+                <span className='text-gradient'>Companies</span> I've Worked
+                with
+            </div>
 
-            <div className="grid grid-cols-4 gap-10 max-lg:grid-cols-3 max-md:grid-cols-2">
-                <img className="img-com-landing" src="/images/brands/bara.png" alt="" />
-                <img className="img-com-landing" src="/images/brands/eksam.png" alt="" />
-                <img className="img-com-landing" src="/images/brands/depotkuota.png" alt="" />
-                <img className="img-com-landing" src="/images/brands/forkeycreative.png" alt="" />
-                <img className="img-com-landing" src="/images/brands/idcpns.png" alt="" />
-                <img className="img-com-landing" src="/images/brands/mizan.png" alt="" />
-                <img className="img-com-landing" src="/images/brands/seremoni.png" alt="" />
+            <div className='grid grid-cols-4 gap-10 max-lg:grid-cols-3 max-md:grid-cols-2'>
+                {companies.map(url => (
+                    <img className='img-com-landing' src={url} alt='' />
+                ))}
             </div>
         </section>
     )
--- constants/brands.ts
+++ constants/brands.ts
@@ -7,5 +7,15 @@ export const brands = [
     '/images/brands/bara.png',
     '/images/brands/idcpns.png',
     '/images/brands/forkeycreative.png',
-    '/images/brands/depotkuota.png'
+    '/images/brands/depotkuota.png',
+]
+export const companies = [
+    '/images/brands/mizan.png',
+    '/images/brands/eksam.png',
+    '/images/brands/seremoni.png',
+    '/images/brands/bara.png',
+    '/images/brands/idcpns.png',
+    '/images/brands/depotkuota.png',
+    '/images/brands/jumpa.png',
+    '/images/brands/kejagung.png'
 ]
