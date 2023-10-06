f57dda8d0bfca821e4812d6dd4797d9ab7ba7e02
adzibilal <adzibilal02@gmail.com>
2023-10-06 16:02:58 +0700

    fixing bugs divider

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -8,62 +8,64 @@ import {
 import { motion } from 'framer-motion'
 export default function Divider() {
     return (
-        <motion.section
-            className='relative h-[200px] overflow-hidden'
-            initial={{ opacity: 0, scale: 1.2 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 1 }}>
-            <div className='bg-gradient-to-r from-blue to-purple w-full h-[100px] absolute rotate-1 scale-110 top-8 max-sm:h-[65px]'></div>
-            <div className='bg-white-primary shadow-lg w-full h-[100px] absolute -rotate-1 scale-110 top-8 flex items-center justify-center gap-10 max-sm:h-[60px] max-sm:gap-5'>
-                <p className='text-divider'>DESIGN</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faBrush} />
-                </p>
-                <p className='text-divider'>DEVELOP</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faCode} />
-                </p>
-                <p className='text-divider'>DISCOVER</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </p>
-                <p className='text-divider'>DESIGN</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faBrush} />
-                </p>
-                <p className='text-divider'>DEVELOP</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faCode} />
-                </p>
-                <p className='text-divider'>DISCOVER</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </p>
-                <p className='text-divider'>DESIGN</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faBrush} />
-                </p>
-                <p className='text-divider'>DEVELOP</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faCode} />
-                </p>
-                <p className='text-divider'>DISCOVER</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </p>
-                <p className='text-divider'>DESIGN</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faBrush} />
-                </p>
-                <p className='text-divider'>DEVELOP</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faCode} />
-                </p>
-                <p className='text-divider'>DISCOVER</p>
-                <p className='text-divider'>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </p>
-            </div>
-        </motion.section>
+        <section className='relative h-[200px] overflow-hidden'>
+            <motion.div
+                className='relative h-[200px]'
+                initial={{ opacity: 0, scale: 1.2 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 1 }}>
+                <div className='bg-gradient-to-r from-blue to-purple w-full h-[100px] absolute rotate-1 scale-110 top-8 max-sm:h-[65px]'></div>
+                <div className='bg-white-primary shadow-lg w-full h-[100px] absolute -rotate-1 scale-110 top-8 flex items-center justify-center gap-10 max-sm:h-[60px] max-sm:gap-5'>
+                    <p className='text-divider'>DESIGN</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faBrush} />
+                    </p>
+                    <p className='text-divider'>DEVELOP</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faCode} />
+                    </p>
+                    <p className='text-divider'>DISCOVER</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </p>
+                    <p className='text-divider'>DESIGN</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faBrush} />
+                    </p>
+                    <p className='text-divider'>DEVELOP</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faCode} />
+                    </p>
+                    <p className='text-divider'>DISCOVER</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </p>
+                    <p className='text-divider'>DESIGN</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faBrush} />
+                    </p>
+                    <p className='text-divider'>DEVELOP</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faCode} />
+                    </p>
+                    <p className='text-divider'>DISCOVER</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </p>
+                    <p className='text-divider'>DESIGN</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faBrush} />
+                    </p>
+                    <p className='text-divider'>DEVELOP</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faCode} />
+                    </p>
+                    <p className='text-divider'>DISCOVER</p>
+                    <p className='text-divider'>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </p>
+                </div>
+            </motion.div>
+        </section>
     )
 }
