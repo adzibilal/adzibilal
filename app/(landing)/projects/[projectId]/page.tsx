02b0dc296fb612dd7b71d2a9ba34cd924cc06dd6
adzibilal <adzibilal02@gmail.com>
2023-12-21 10:56:56 +0700

    landing detail project

--- app/(landing)/projects/[projectId]/page.tsx
+++ app/(landing)/projects/[projectId]/page.tsx
@@ -5,49 +5,25 @@ import { NextPage } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import Head from 'next/head';
+import Head from 'next/head'
+import axios from 'axios'
+import { Project } from '@prisma/client'
+import parse from 'html-react-parser'
 
-
-interface Project {
-    _id: string
-    judul: string
-    deskripsi: string
-    content: string
-    teknologi: string[]
-    link: string
-    image: string[]
-}
 
 export default function Page({ params }: { params: { projectId: string } }) {
     const projectId = params.projectId
 
-    const [project, setProject] = useState<Project | null>(null)
+    const [project, setProject] = useState<Project>()
+
+    const getProject = async () => {
+        if (!projectId) return
+        const res = await axios.get(`/api/project/${projectId}`)
+        return setProject(res.data)
+    }
 
     useEffect(() => {
-        if (projectId) {
-            // Buat permintaan GET ke API Anda dengan projectId yang diambil dari URL
-            fetch(`/api/projects/?projectId=${projectId}`, {
-                method: 'GET'
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok')
-                    }
-
-                    return response.json()
-                })
-                .then(data => {
-                    console.error(data)
-                    // Setel data proyek ke dalam state
-                    setProject(data)
-                })
-                .catch(error => {
-                    console.error(
-                        'There was a problem with the fetch operation:',
-                        error
-                    )
-                })
-        }
+        getProject()
     }, [projectId])
 
     if (!project) {
@@ -63,13 +39,13 @@ export default function Page({ params }: { params: { projectId: string } }) {
         <div className='max-container max-lg:!mx-5 min-h-[80vh] !mt-10'>
             <Head>
                 {/* Meta Gambar */}
-                <meta property='og:image' content={project.image[0]} />
+                <meta property='og:image' content={project.images[0]} />
 
                 {/* Judul */}
-                <title>{project.judul} - Portofolio Adzi Bilal</title>
+                <title>{project.title} - Portofolio Adzi Bilal</title>
 
                 {/* Deskripsi */}
-                <meta name='description' content={project.deskripsi} />
+                <meta name='description' content={project.description ?? ''} />
             </Head>
             <div className='text-sm breadcrumbs mb-5'>
                 <ul>
@@ -79,13 +55,13 @@ export default function Page({ params }: { params: { projectId: string } }) {
                     <li>
                         <Link href='/projects'>Projects</Link>
                     </li>
-                    <li>{project.judul}</li>
+                    <li>{project.title}</li>
                 </ul>
             </div>
             <div className='grid grid-cols-[1fr_1.5fr] gap-5 max-md:grid-cols-1 mb-20'>
                 <div className=''>
                     <div className='carousel w-full'>
-                        {project.image.map((url, index) => (
+                        {project.images.map((url, index) => (
                             <div
                                 id={`${index}`}
                                 key={index}
@@ -101,7 +77,7 @@ export default function Page({ params }: { params: { projectId: string } }) {
                         ))}
                     </div>
                     <div className='flex justify-center w-full gap-1'>
-                        {project.image.map((url, index) => (
+                        {project.images.map((url, index) => (
                             <a href={`#${index}`} key={index}>
                                 <Image
                                     width={120}
@@ -117,14 +93,14 @@ export default function Page({ params }: { params: { projectId: string } }) {
 
                 <div className=''>
                     <div className='title-section text-gradient'>
-                        {project.judul}
+                        {project.title}
                     </div>
                     <div className='text-black-primary dark:text-gray mb-4'>
-                        {project.deskripsi}
+                        {project.description}
                     </div>
 
                     <div className='tech flex gap-2 mb-3'>
-                        {project.teknologi.map((tech, index) => (
+                        {project.tech.map((tech, index) => (
                             <div
                                 className='bg-gray-light dark:bg-white-secondary text-black-primary w-max px-3 py-1 rounded-full text-xs'
                                 key={index}>
@@ -134,15 +110,17 @@ export default function Page({ params }: { params: { projectId: string } }) {
                     </div>
 
                     <div className='text-justify text-sm mb-5'>
-                        {project.content}
+                        {parse(project.content ?? 'Content Belum di set')}
                     </div>
 
-                    <Link href={project.link} className='btn btn-info'>
-                        Live Project
-                    </Link>
+                    {project.link && (
+                        <Link href={project.link} className='btn btn-info'>
+                            Live Project
+                        </Link>
+                    )}
                 </div>
             </div>
-            <ProjectComments projectId={projectId} />
+            {/* <ProjectComments projectId={projectId} /> */}
             <CtaFooter /> <br />
             <br />
         </div>
