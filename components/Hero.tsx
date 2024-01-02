82ea4977baa89ab734790a84e2c750b0cd06a0d7
adzibilal <adzibilal02@gmail.com>
2024-01-02 10:13:06 +0700

    change hero img

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,44 @@ import { motion } from 'framer-motion'
 
 export default function Hero() {
     return (
-        <section className='hero'>
+        <section className='max-container grid grid-cols-[1.5fr_1fr] gap-2 items-center max-sm:flex max-sm:flex-col-reverse max-sm:p-5'>
+            <div className=''>
+                <motion.h1
+                    className='hero-title sm:!text-left'
+                    initial={{ opacity: 0, y: 20 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, delay: 0.2 }}>
+                    <span>Hello, I'm Adzi Bilal M H,</span> <br /> Frontend
+                    Developer based in Indonesia
+                </motion.h1>
+                <motion.p
+                    className='sub-hero sm:!text-left sm:w-full'
+                    initial={{ opacity: 0, y: 20 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, delay: 0.5 }}>
+                    As a dedicated Frontend Developer, I'm passionate about
+                    crafting innovative and user-centric web experiences for
+                    global audiences.
+                </motion.p>
+
+                <motion.div
+                    className='flex gap-5 mt-5 max-sm:flex-col'
+                    initial={{ opacity: 0, y: 20 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, delay: 0.6 }}>
+                    <Link
+                        href='https://wa.link/e051oz'
+                        target='_blank'
+                        className='button button-lg !rounded-full'>
+                        GET IN TOUCH
+                    </Link>
+                    <Link
+                        href='/projects'
+                        className='button-outline button-lg !rounded-full'>
+                        VIEW ALL WORKS
+                    </Link>
+                </motion.div>
+            </div>
             <motion.div
                 className=''
                 initial={{ opacity: 0, y: 20 }}
@@ -13,48 +50,12 @@ export default function Hero() {
                 transition={{ duration: 1 }}>
                 <Image
                     className='rounded-full'
-                    src='/images/ava-adzi.png'
+                    src='/images/img-adzi.png'
                     alt='Adzi Logo'
-                    width={200}
-                    height={20}
+                    width={1000}
+                    height={1000}
                     priority
                 />
-            </motion.div>
-
-            <motion.h1
-                className='hero-title'
-                initial={{ opacity: 0, y: 20 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.2 }}>
-                <span>Hello, I'm Adzi Bilal M H,</span> <br /> Frontend
-                Developer based in Indonesia
-            </motion.h1>
-            <motion.p
-                className='sub-hero'
-                initial={{ opacity: 0, y: 20 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.5 }}>
-                As a dedicated Frontend Developer, I'm passionate about crafting
-                innovative and user-centric web experiences for global
-                audiences.
-            </motion.p>
-
-            <motion.div
-                className='flex gap-5 mt-5 max-sm:flex-col'
-                initial={{ opacity: 0, y: 20 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.6}}>
-                <Link
-                    href='https://wa.link/e051oz'
-                    target='_blank'
-                    className='button button-lg !rounded-full'>
-                    GET IN TOUCH
-                </Link>
-                <Link
-                    href='/projects'
-                    className='button-outline button-lg !rounded-full'>
-                    VIEW ALL WORKS
-                </Link>
             </motion.div>
         </section>
     )
